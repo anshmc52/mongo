7e8f8cb9a3e5766f704d00168d6c31e5be9df6a9
ansh <anshmc52@gmail.com>
2020-10-30 19:47:09 +0300

    рабочая версия

--- mongo_install.sh
+++ mongo_install.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-nano /etc/yum.repos.d/mongodb-org-4.2.repo
+#nano /etc/yum.repos.d/mongodb-org-4.2.repo
+#cp ./mongodb-org-4.2.repo /etc/yum.repos.d/
 
 #insert content below in repository 
+
+cat > /etc/yum.repos.d/mongodb-org-4.2.repo <<EOF
 [mongodb-org-4.2]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
+EOF
 
+# install
 yum repolist
 yum -y install mongodb-org
 systemctl enable mongod
 systemctl start mongod
+
+# copy config and restart
+
+cp ./mongod.conf /etc/
+systemctl restart mongod
